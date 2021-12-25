ba55df3483f425f22d479a6ab4091ff31b0ff2a1
GEbi Beshir <gebi9@hotmail.com>
2021-12-25 11:10:11 +0100

    inited project: via npx

---
+++ src/app/components/App.jsx
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import React from "react";
+import {ListDataComponent} from "./ListDataComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <ListDataComponent/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/app/components/ListDataComponent.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import '../styles/ListDataComponentStyle.scss';
+
+export const ListDataComponent = () => {
+    return (
+        <div className={"container"}>
+            <h2>List of Data</h2>
+        </div>
+    );
+};
